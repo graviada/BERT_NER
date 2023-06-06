127bf308dbf022b6c9724b843fc89fb530144e9a
Irina Chalova <irish.moscow@gmail.com>
2023-06-06 16:00:15 +0300

    Внесение правок в файл main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import copy
 from pathlib import Path
 from functools import partial
 from argparse import ArgumentParser
@@ -23,11 +24,12 @@ LOGGING_PATH = '/content/result/logging'
 def main(data_name, model_name, result_dir, num_epochs, max_length, batch_size, 
     learning_rate, dropout, weight_decay):
     logging_dir = Path(result_dir).joinpath('logging')
-    print(logging_dir)
+    print('Директория логгирования:', logging_dir)
 
     # Выгрузка данных и словарей маппинга для меток и айдишников
     if data_name == 'runne':
         datadict = load_dataset(DATA_NER[data_name])
+        datadict = datadict['dev'].rename('valid')
         label2id = Runne.LABEL2ID
     else:
         datadict = load_dataset(DATA_NER[data_name][0], DATA_NER[data_name][1])
@@ -38,25 +40,28 @@ def main(data_name, model_name, result_dir, num_epochs, max_length, batch_size,
 
     id2label = {v: k for k, v in label2id.items()}
     config = DATA_TO_CLASS[data_name](datadict)
+    print('Инициализирован config:', config)
 
     # Инициализация датаколлатора и токенизатора
-    tokenizer = BERTModelNER.get_tokenizer(model_name=model_name)
+    tokenizer = BERTModelNER.get_tokenizer(model_name=MODEL_TO_HUB_NAME[model_name])
     data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
 
     # Обработка данных токенизатором
     tokenized_datadict = datadict.map(
         lambda example: config.process_data(example, tokenizer, max_length),
         batched=True)
+    print('Данные обработаны токенизатором')
 
     # Инициализация модели
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     model = BERTModelNER.model_initialization(
-        model_name=model_name,
+        model_name=MODEL_TO_HUB_NAME[model_name],
         id2label=id2label,
         label2id=label2id,
         num_labels=len(label2id)
         ).to(device)
-    
+    print(f'Модель {model_name} загружена')
+
     # Инициализация трейнера и запуск обучения
     trainer = Trainer(
         model=model,
@@ -84,7 +89,7 @@ def main(data_name, model_name, result_dir, num_epochs, max_length, batch_size,
 # Для задания параметров обучения из командной строки
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--data_name', required=True, choices=DATA_NER.keys())
+    parser.add_argument('--data-name', required=True, choices=DATA_NER.keys())
     parser.add_argument('--model-name', required=True, choices=MODEL_TO_HUB_NAME.keys())
     parser.add_argument('--result-dir', required=True, type=Path)
     parser.add_argument('--num-epochs', type=int, default=5)
